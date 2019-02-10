a69e485dabb47fa740995406c3e7b5974eff4d32
StrelnikovVitaliy <strelnikov93vitaliy@gmail.com>
2019-02-10 23:53:19 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,1 @@
+var slideIndex=1;function plusDivs(e){showDivs(slideIndex+=e)}function showDivs(e){var s,l=document.getElementsByClassName("carousel__slide");for(e>l.length&&(slideIndex=1),e<1&&(slideIndex=l.length),s=0;s<l.length;s++)l[s].style.display="none";l[slideIndex-1].style.display="block"}showDivs(slideIndex);
